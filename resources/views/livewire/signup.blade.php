0dec4e8f1e6cceaa72788bb8a5d73c050772df54
Laura Imanuelle <lauradalopez@gmail.com>
2022-10-29 23:00:07 +0700

    create signup input fields

--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -2,4 +2,42 @@
     <div class="justify-center flex">
         <p class="text-4xl text-white font-semibold mb-[70px]">Create an account.</p>
     </div>
+    <div class="gap-y-8 grid grid-cols-auto">
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">First Name</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">Last Name</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">Email Address</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">Phone Number</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">Password</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">Confirm Password</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+    </div>
 </div>
