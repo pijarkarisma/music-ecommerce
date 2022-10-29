57b256335a4ab752a516f3173fd003e8c0574cf1
Pijar Karisma <pijarkarisma@gmail.com>
2022-10-29 07:18:37 +0700

    set responsiveness for small screen

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,6 +1,6 @@
 <div class="container m-0 p-0">
     <div class="row m-0 p-0" style="width: 100vw; height: 100vh">
-        <div class="col d-flex m-0 p-0">
+        <div class="col d-none d-md-flex m-0 p-0">
             <img src="/images/landing_back_image.png" alt="landing_background_image" style="object-fit: cover; width: 100%; height: 100vh">
         </div>
         <div class="col text-center d-flex align-items-center justify-content-center" style="background-color:#1D1D1D">
@@ -12,13 +12,13 @@
                     <h1 class="mt-4" style="color: white">Welcome to<br>Karisma Music Store!</h1>
                 </div>
                 <div class="row d-flex justify-content-center" style="margin-top: 5rem">
-                    <a type="button" class="btn text-center" style="border-radius: 60px; background-color: #951D1D; height: 60px; width: 270px">
+                    <a type="button" class="btn text-center" style="border-radius: 60px; background-color: #951D1D; height: 60px; width: 12rem">
                         <p style="font-weight:400; color:white; font-size:25px">Sign Up</p>
                     </a>
                 </div>
 
                 <div class="row d-flex justify-content-center" style="margin-top: 1.5rem">
-                    <a type="button" class="btn text-center" style="border-radius: 60px; background-color: #615050; height: 60px; width: 270px">
+                    <a type="button" class="btn text-center" style="border-radius: 60px; background-color: #615050; height: 60px; width: 12rem">
                         <p style="font-weight:400; color:white; font-size:25px">Log In</p>
                     </a>
                 </div>
