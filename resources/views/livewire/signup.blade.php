f2841ffded60700f9f1cf324b40db8a6874f6ca4
Laura Imanuelle <lauradalopez@gmail.com>
2022-10-29 23:04:44 +0700

    create signup page overflow

--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-zinc-900 h-screen p-16 flex flex-col justify-items-center">
+<div class="bg-zinc-900 h-screen p-16 flex flex-col justify-items-center overflow-y-auto">
     <div class="justify-center flex">
         <p class="text-4xl text-white font-semibold mb-[70px]">Create an account.</p>
     </div>
