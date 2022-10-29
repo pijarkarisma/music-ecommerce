bb135920c7d928380bb39264ade77679196b387b
Laura Imanuelle <lauradalopez@gmail.com>
2022-10-29 22:48:39 +0700

    create signup title

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Login;
+use App\Http\Livewire\Signup;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Welcome;
 
@@ -19,4 +20,4 @@ use App\Http\Livewire\Welcome;
 //     return view('livewire.landing-page');
 // });
 
-Route::get('/', Login::class);
+Route::get('/', Signup::class);
