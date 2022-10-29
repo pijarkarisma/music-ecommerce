3df3b588b3e65313ed01b65d2978c63a5ae046eb
Laura Imanuelle <lauradalopez@gmail.com>
2022-10-29 18:18:34 +0700

    create login input e-mail

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -2,4 +2,12 @@
     <div class="justify-center flex">
         <p class="text-4xl text-white font-semibold mb-[70px]">Log into existing account.</p>
     </div>
+    <div class="gap-y-10 grid grid-cols-auto">
+        <div class="justify-center flex">
+            <div class="w-1/3 grid">
+                <p class="text-xl text-white mb-1">E-mail Address</p>
+                <input type="text" class="w-full h-8">
+            </div>
+        </div>
+    </div>
 </div>
