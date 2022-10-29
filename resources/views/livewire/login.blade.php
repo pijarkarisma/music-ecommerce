fa4198b5a1cde15ec37040b99b76e8adad82c0d9
Laura Imanuelle <lauradalopez@gmail.com>
2022-10-29 18:15:44 +0700

    create login title

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-zinc-900 h-screen p-16 flex flex-col justify-items-center">
+<div class="bg-zinc-900 h-screen p-36 flex flex-col justify-items-center">
     <div class="justify-center flex">
-        <p class="text-4xl text-white font-semibold mb-[70px]">Create an account.</p>
+        <p class="text-4xl text-white font-semibold mb-[70px]">Log into existing account.</p>
     </div>
 </div>
