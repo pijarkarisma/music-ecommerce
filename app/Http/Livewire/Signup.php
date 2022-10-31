a932a70a1f72688ae02bb30d3a41e6cfcb703c22
Error512 <keivan.roin09@gmail.com>
2022-10-31 13:58:35 +0700

    penambahan minor login

--- app/Http/Livewire/Signup.php
+++ app/Http/Livewire/Signup.php
@@ -3,11 +3,42 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Fascades\Auth;
+use App\Model\User;
+use app\providers\RouteServiceProvider;
 
 class Signup extends Component
 {
+    public $first_name,$last_name,$email,$phone,$password,$password_confirmation;
     public function render()
     {
         return view('livewire.signup')->extends('layouts.app');
+    }
+
+    public function rules(){
+        return[
+            'first_name'=>['required'],
+            'last_name'=>['required'],
+            'email'=>['required','email','unique:users'],
+            'phone'=>['required'],
+            'password'=>['required','confirmed'],
+        ];
+    }
+ 
+
+    public function registerUser(){
+        
+        $this->validate();
+
+        $user = User::create([
+        'first_name' => $this->first_name,
+        'last_name' => $this->last_name,
+        'email' => $this->email,
+        'phone' => $this->phone,
+        'password' => bcrypt($this->password)
+
+        ]);
+        Auth:login($user,true);
+        return redirect()->to('/login');
     }
 }
--- resources/views/livewire/signup.blade.php
+++ resources/views/livewire/signup.blade.php
@@ -2,47 +2,49 @@
     <div class="justify-center flex">
         <p class="text-4xl text-white font-semibold mb-[70px]">Create an account.</p>
     </div>
+   <form wire:submit.prevent="registerUser">
     <div class="gap-y-8 grid grid-cols-auto">
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">First Name</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="first_name">
             </div>
         </div>
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">Last Name</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="last_name">
             </div>
         </div>
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">Email Address</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="email">
             </div>
         </div>
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">Phone Number</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="phone">
             </div>
         </div>
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">Password</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="password">
             </div>
         </div>
         <div class="justify-center flex">
             <div class="w-1/3 grid">
                 <p class="text-xl text-white mb-1">Confirm Password</p>
-                <input type="text" class="w-full h-8">
+                <input type="text" class="w-full h-8" id="password_confirmation">
             </div>
         </div>
         <div class="justify-center flex mt-11">
-            <a type="button" class="flex h-14 w-56 lg:w-64 text-center bg-music-red items-center justify-center rounded-[60px]">
-                <p class="text-xl text-white">Sign Up</p>
+            <button type="submit" class="flex h-14 w-56 lg:w-64 text-center bg-music-red items-center justify-center rounded-[60px] text-white">Sign Up</button>
+
             </a>
         </div>
+    </form>
     </div>
 </div>
